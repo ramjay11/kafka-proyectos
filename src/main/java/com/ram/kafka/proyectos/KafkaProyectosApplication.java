02875a00321a7e45dcf15c563536b1c98af5ef97
Romorosa <ramjay.romorosa@accenture.com>
2022-05-18 01:01:04 +0800

    initial committing!

---
+++ src/main/java/com/ram/kafka/proyectos/KafkaProyectosApplication.java
@@ -0,0 +1,13 @@
+package com.ram.kafka.proyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProyectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProyectosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ram/kafka/proyectos/KafkaProyectosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ram.kafka.proyectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProyectosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
